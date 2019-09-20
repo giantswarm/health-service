edbbd13120d80fedae95e8ae980274a4b7893257
Thomas Fussell <thomas@fussell.io>
2019-09-20 12:08:10 -0400

    remove response defaults

--- service/health/searcher/azure.go
+++ service/health/searcher/azure.go
@@ -2,25 +2,13 @@ package searcher
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/giantswarm/microerror"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// searchAWSCR searches for the cluster config in AWSClusterConfigs resources.
+// searchAzureCR searches for the cluster config in AWSClusterConfigs resources.
 func (s *Service) searchAzureCR(ctx context.Context, request Request) (*Response, error) {
-	awsClusterConfigName := fmt.Sprintf("%s-%s", request.ClusterID, "aws-cluster-config")
-	awsClusterConfig, err := s.G8sClient.CoreV1alpha1().AWSClusterConfigs(metav1.NamespaceDefault).Get(awsClusterConfigName, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		return nil, microerror.Mask(clusterNotFoundError)
-	} else if err != nil {
-		return nil, microerror.Mask(err)
+	response := Response{
+		ClusterHealth: "green",
 	}
-
-	response := DefaultResponse()
-	response.ClusterHealth = awsClusterConfig.GetName()
 
 	return &response, nil
 }
