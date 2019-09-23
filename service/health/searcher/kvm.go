dab96754ee231d3375fae2a787a9debecee127f7
Thomas Fussell <thomas@fussell.io>
2019-09-23 14:51:54 -0400

    revert basic kvm health check, moved to a new branch

--- service/health/searcher/kvm.go
+++ service/health/searcher/kvm.go
@@ -2,30 +2,12 @@ package searcher
 
 import (
 	"context"
-
-	"github.com/giantswarm/microerror"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // searchKVMCR searches for the cluster config in AWSClusterConfigs resources.
 func (s *Service) searchKVMCR(ctx context.Context, request Request) (*Response, error) {
-	kvmCR, err := s.G8sClient.ProviderV1alpha1().KVMConfigs("default").Get(request.ClusterID, v1.GetOptions{})
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-	var health string
-	{
-		nodeCount := len(kvmCR.Status.Cluster.Nodes)
-		if nodeCount == 4 {
-			health = "green"
-		} else if nodeCount > 0 {
-			health = "yellow"
-		} else {
-			health = "red"
-		}
-	}
 	response := Response{
-		ClusterHealth: health,
+		ClusterHealth: "green",
 	}
 
 	return &response, nil
