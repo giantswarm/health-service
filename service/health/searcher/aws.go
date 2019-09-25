a8e6636190b02e7e2d61c52b2b5c2350d2ff6781
Zach Stone <zach@giantswarm.io>
2019-09-25 10:19:32 +0200

    WIP commit for rebase

--- service/health/searcher/aws.go
+++ service/health/searcher/aws.go
@@ -2,13 +2,20 @@ package searcher
 
 import (
 	"context"
+	"fmt"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // searchAWSCR searches for the cluster config in AWSClusterConfigs resources.
 func (s *Service) searchAWSCR(ctx context.Context, request Request) (*Response, error) {
 	response := Response{
-		ClusterHealth: "green",
+		ClusterHealth: "greenio",
 	}
+	// Use clusteroperator snippet as a model to create k8s client
+	// Use Go client for K8s to query AWSClusterConfigs
+	awsCR, err := s.G8sClient.ProviderV1alpha1().AWSConfigs("default").Get(request.ClusterID, v1.GetOptions{})
 
+	fmt.Println(awsCR, err)
 	return &response, nil
 }
