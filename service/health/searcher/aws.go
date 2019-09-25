7aa41690a3a3fae2e6a8496aeffeba3c3bcf3e65
Zach Stone <zach@giantswarm.io>
2019-09-25 18:28:11 +0200

    Use apiextensions helper functions for checking Conditions

--- service/health/searcher/aws.go
+++ service/health/searcher/aws.go
@@ -3,29 +3,10 @@ package searcher
 import (
 	"context"
 
+	"github.com/giantswarm/health-service/service/health/key"
+
 	"github.com/giantswarm/microerror"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-// AWSGreenConditions contains the values for a cluster state which are considered 'green'
-var AWSGreenConditions = map[string]struct{}{
-	"Creating": {}, "Created": {}, "Updated": {}}
-
-// AWSYellowConditions contains the values for a cluster state which are considered 'yellow'
-var AWSYellowConditions = map[string]struct{}{
-	"Scaling": {}, "Draining": {}, "Drained": {}, "Updating": {}}
-
-// AWSRedConditions contains the values for a cluster state which are considered 'red'
-var AWSRedConditions = map[string]struct{}{
-	"Deleting": {}, "Deleted": {}}
-
-const (
-	// HealthRed represents an unhealthy cluster
-	HealthRed = "red"
-	// HealthYellow represents a passable cluster
-	HealthYellow = "yellow"
-	//HealthGreen represents a healthy cluster
-	HealthGreen = "green"
 )
 
 // searchAWSCR searches for the cluster config in AWSClusterConfigs resources.
@@ -36,36 +17,24 @@ func (s *Service) searchAWSCR(ctx context.Context, request Request) (*Response, 
 		return nil, microerror.Mask(err)
 	}
 
-	var clusterHealth string = HealthGreen // Optimistic default
+	var clusterHealth = key.HealthGreen // Optimistic default
 
+	// TODO: Move to separate function
 	// Check desired/actual node counts
 	var desiredNodes int = awsCR.Status.Cluster.Scaling.DesiredCapacity
 	var currentNodes int = len(awsCR.Status.Cluster.Nodes)
 
 	if currentNodes < desiredNodes {
-		clusterHealth = HealthYellow
+		clusterHealth = key.HealthYellow
 	}
 
-	// Examine Cluster conditions and use the worst as my state
-	for _, condition := range awsCR.Status.Cluster.Conditions {
-
-		_, isGreen := AWSGreenConditions[condition.Type]
-		if isGreen {
-			// Don't set back to green from Yellow or Red state
-		}
-
-		_, isYellow := AWSYellowConditions[condition.Type]
-		if isYellow {
-			// Set health to Yellow if it isn't already Red
-			if clusterHealth != HealthRed {
-				clusterHealth = HealthYellow
-			}
-		}
-
-		_, isRed := AWSRedConditions[condition.Type]
-		if isRed {
-			clusterHealth = HealthRed
-		}
+	var clusterStatus = awsCR.ClusterStatus()
+	if clusterStatus.HasDeletedCondition() || clusterStatus.HasDeletingCondition() {
+		clusterHealth = key.HealthRed
+	} else if clusterStatus.HasUpdatingCondition() || clusterStatus.HasCreatingCondition() { // TODO: Account for draining/scaling
+		clusterHealth = key.HealthYellow
+	} else if clusterStatus.HasCreatedCondition() || clusterStatus.HasUpdatedCondition() {
+		// Don't set back to Green from Yellow or Red
 	}
 
 	response := Response{
