5fd99a3dee682d2b604bfa3535334a27ab9e9c09
Zach Stone <zach@giantswarm.io>
2019-09-26 15:37:25 +0200

    Add sample AWS health service (#8)
    * Add sample AWS health check service

--- service/health/key/key.go
+++ service/health/key/key.go
@@ -1,5 +1,16 @@
 package key
 
+const (
+	// HealthRed represents an unhealthy cluster
+	HealthRed = "red"
+	// HealthYellow represents a passable cluster
+	HealthYellow = "yellow"
+	//HealthGreen represents a healthy cluster
+	HealthGreen = "green"
+	// HealthDefault is used when a condition cannot otherwise be determined
+	HealthDefault = "green"
+)
+
 var Providers = [...]string{"aws", "azure", "kvm"}
 
 func IsKnownProvider(value string) bool {
