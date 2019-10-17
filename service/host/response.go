ab2a34ffe5381428b5b90142868a7d90775452bd
Zach Stone <zach@giantswarm.io>
2019-10-16 17:45:13 -0700

    Change json name of spec field

--- service/host/response.go
+++ service/host/response.go
@@ -6,7 +6,7 @@ import v1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 type Response struct {
 	Endpoint string                 `json:"endpoint"`
 	Status   v1alpha1.StatusCluster `json:"cluster"`
-	Spec     ProviderSpec           `json:"provider"`
+	Spec     ProviderSpec           `json:"provider_spec"`
 }
 
 type ProviderSpec struct {
